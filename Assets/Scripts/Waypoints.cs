e7f1f6c085392894b990f88ecd7eb1fce0fc8824
El-Parto <luc980@y7mail.com>
2021-05-05 17:55:30 +1000

    Pertaining to setting the scene up with way and wait points

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    public Vector3 Position => transform.position;
+
+
+
+private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.1f);
+    }
+}
