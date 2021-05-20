bd60c42d96fb14d32a467613fddb43e528847158
El-Parto <luc980@y7mail.com>
2021-05-20 13:00:27 +1000

    Superficial debugging

--- Assets/Scripts/AgentMaxwell.cs
+++ Assets/Scripts/AgentMaxwell.cs
@@ -23,11 +23,37 @@ public class AgentMaxwell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debugging();
+
+        //Logs out if a new path has been set - checking for "freezing" on path select
+        if (agentMax.pathPending)
+            Debug.LogWarning("Going to new path");
+
+        if (agentMax.pathStatus == NavMeshPathStatus.PathPartial)
+        {
+            // if the status of the path is invalid, set a new destination
+            agentMax.SetDestination(RandomPoint.Position);
+            Debug.LogWarning("The path was partial, and i chose a new destination");
+        }
         // Has the agent reached it's position?
-        if (!agentMax.pathPending && agentMax.remainingDistance < 0.1f)
+        if (!agentMax.pathPending && agentMax.remainingDistance < 0.25f)
         {
             // Tell the agent to move to a random position in the scene waypoints
             agentMax.SetDestination(RandomPoint.Position);
         }
     }
+
+    private void Debugging()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (agentMax.isPathStale == true)
+            {
+                Debug.LogError("It'll never work!");
+            }
+            else
+                Debug.Log("But it's certainly worth a try!?");
+        }
+    }
+
 }
